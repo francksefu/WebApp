a502f868276ab5ffa098f4684a4bf8603857b2cb
francksefu <francksefu1998@gmail.com>
2023-09-24 01:05:14 +0200

    Correct linter

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_many :posts, foreign_key: 'author_id'
-	has_many :comments, foreign_key: 'author_id'
-	has_many :likes, foreign_key: 'author_id'
+  has_many :comments, foreign_key: 'author_id'
+  has_many :likes, foreign_key: 'author_id'
 
   def recent_post
     Post.where(author_id: id).limit(3).order(created_at: :desc)
