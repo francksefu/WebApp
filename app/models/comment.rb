83f0fa87adac28bcd9c31ae48d3f11e77960a3ea
francksefu <francksefu1998@gmail.com>
2023-09-24 00:54:47 +0200

    Fix issues

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'user_id'
-  belongs_to :posts, class_name: 'Post', foreign_key: 'post_id'
+  belongs_to :post, class_name: 'Post', foreign_key: 'post_id'
 
   def update_comment_counter
     num = Post.find_by(id: post_id).comments_counter.to_i
--- app/models/like.rb
+++ app/models/like.rb
@@ -1,6 +1,6 @@
 class Like < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'user_id'
-  belongs_to :posts, class_name: 'Post', foreign_key: 'post_id'
+  belongs_to :post, class_name: 'Post', foreign_key: 'post_id'
 
   def update_like_counter
     num = Post.find_by(id: post_id).likes_counter.to_i
