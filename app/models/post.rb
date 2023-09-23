c861f09a422f384b113ede912dbf39b2163b3249
francksefu <francksefu1998@gmail.com>
2023-09-23 23:32:32 +0200

    Correct linter

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,13 +1,13 @@
 class Post < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
-	has_many :comments
+  has_many :comments
 
-	def update_post_counter
-		num = User.find_by(id: author_id).posts_counter.to_i
-		User.find_by(id: author_id).update(posts_counter: num + 1)
-	end
+  def update_post_counter
+    num = User.find_by(id: author_id).posts_counter.to_i
+    User.find_by(id: author_id).update(posts_counter: num + 1)
+  end
 
-	def recent_comments
-		Comment.where(post_id: id).limit(5).order(created_at: :desc)
-	end
+  def recent_comments
+    Comment.where(post_id: id).limit(5).order(created_at: :desc)
+  end
 end
