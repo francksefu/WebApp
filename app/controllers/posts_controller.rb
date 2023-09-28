affee4509c0917a3606812753418d65fae5dbb4d
francksefu <francksefu1998@gmail.com>
2023-09-28 11:48:30 +0200

    Correct linter

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,22 +1,23 @@
 class PostsController < ApplicationController
-	#Create action - Handle the creation of a new post
+  # Create action - Handle the creation of a new post
 
-	def create
-		@user = User.find(params[:author_id])
-		@post = @user.posts.create(post_params)
-		redirect_to user_path(@user)
-	end
+  def create
+    @user = User.find(params[:author_id])
+    @post = @user.posts.create(post_params)
+    redirect_to user_path(@user)
+  end
 
-	def index
-		@post = Post.all
-	end
+  def index
+    @post = Post.all
+  end
 
-	def show
-		@post = Post.find(params[:id])
-	end
+  def show
+    @post = Post.find(params[:id])
+  end
 
-	private
-	def post_params
-		params.require(:post).permit(:text)
-	end
+  private
+
+  def post_params
+    params.require(:post).permit(:text)
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,12 @@
 class UsersController < ApplicationController
-	#index action
-	def index
-		@users = User.all
-	end
+  # index action
+  def index
+    @users = User.all
+  end
 
-	def show
-		@user = User.find(params[:id])
-	end
+  def show
+    @user = User.find(params[:id])
+  end
 
-	#We will add other action after
+  # We will add other action after
 end
