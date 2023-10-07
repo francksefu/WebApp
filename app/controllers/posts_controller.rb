56ba2ca25d3ef5520934123f8070098c12bf1876
francksefu <francksefu1998@gmail.com>
2023-10-07 02:08:05 +0200

    Implements the views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,11 +2,17 @@ class PostsController < ApplicationController
   # Create action - Handle the creation of a new post
 
   def index
-    @post = Post.all
+    @post = Post.where(author_id: params[:user_id])
+    @comment = Comment.all
+    @user = User.find_by(id: params[:user_id])
+    @user_check = User.all
   end
 
   def show
-    @post = Post.find(params[:id])
+    @post = Post.find_by(id: params[:id])
+    @comment = Comment.all
+    @user = User.find_by(id: params[:user_id])
+    @user_check = User.all
   end
 
   private
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @post = Post.where(author_id: params[:id])
   end
 
   # We will add other action after
