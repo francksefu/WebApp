1fc212c44410b23ec6e36a8b47e290cbbb250b8b
francksefu <francksefu1998@gmail.com>
2023-10-08 04:08:49 +0200

    correct linters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,8 @@
 # app/controllers/application_controller.rb
 class ApplicationController < ActionController::Base
-    helper_method :current_user
-  
-    def current_user
-      @current_user ||= User.first
-    end
+  helper_method :current_user
+
+  def current_user
+    @current_user ||= User.first
   end
-  
+end
