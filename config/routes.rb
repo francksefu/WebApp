f6920f577cef6130e81b70452be1e90b85e2d147
francksefu <francksefu1998@gmail.com>
2023-10-15 01:44:03 +0200

    Update migrate directory and correct router

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   devise_for :users
   # Defines the root path route ("/")
-  # root "articles#index"
+   root "users#index"
   resources :users do
     resources :posts  # Define the route for the user's posts
   end
